eeba66f2b14b13abc2b63e8262792f5b2248e70e
Caglar Kutlu <caglar.kutlu@zhinst.com>
2024-03-31 17:39:38 +0200

    add tmux.conf and fish configuration

---
+++ dot_config/private_fish/conf.d/abbr.fish
@@ -0,0 +1,5 @@
+abbr --add tmxt "tmux attach -t"
+abbr --add tmxl "tmux list-s"
+abbr --add fzf "fzf --height 35%"
+
+abbr --add clean_nb_output "jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace"
---
+++ dot_config/private_fish/conf.d/common.fish
@@ -0,0 +1,6 @@
+# No greeting message
+set -g fish_greeting
+
+# Variables
+set -U EDITOR vim
+set -U SUDO_EDITOR /usr/bin/vim
