e5ac2850d713f2c64a388dc35bfdc3407681b683
Caglar Kutlu <caglar.kutlu@zhinst.com>
2024-05-05 15:28:49 +0200

    update lazylock & abbreviations

--- dot_config/private_fish/conf.d/abbr.fish
+++ dot_config/private_fish/conf.d/abbr.fish
@@ -3,3 +3,5 @@ abbr --add tmxl "tmux list-s"
 abbr --add fzf "fzf --height 35%"
 
 abbr --add clean_nb_output "jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace"
+
+abbr --add learnrust "cd $HOME/Playground/rust/rust-book/ && tmux new-s -ADs learnrust"
