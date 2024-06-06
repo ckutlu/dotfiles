928f685c2220ab0018549e9cd71adc2bbff8340c
Caglar Kutlu <caglar.kutlu@zhinst.com>
2024-06-06 08:41:44 +0200

    add rye abbreviation

--- dot_config/private_fish/conf.d/abbr.fish
+++ dot_config/private_fish/conf.d/abbr.fish
@@ -4,3 +4,4 @@ abbr --add fzf "fzf --height 35%"
 abbr --add bat batcat
 abbr --add clean_nb_output "jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace"
 abbr --add learnrust "cd $HOME/Playground/rust/rust-book/ && tmux new-s -ADs learnrust"
+abbr --add rytest "rye run pytest"
