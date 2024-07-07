0728eb45f743a24bbb4a6c820512174a6db3e8ae
Caglar Kutlu <caglar.kutlu@gmail.com>
2024-07-07 16:27:25 +0200

    Remove batcat and rytest abbreviations.

--- dot_config/private_fish/conf.d/abbr.fish
+++ dot_config/private_fish/conf.d/abbr.fish
@@ -1,7 +1,5 @@
 abbr --add tmxt "tmux attach -t"
 abbr --add tmxl "tmux list-s"
 abbr --add fzf "fzf --height 35%"
-abbr --add bat batcat
 abbr --add clean_nb_output "jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace"
 abbr --add learnrust "cd $HOME/Playground/rust/rust-book/ && tmux new-s -ADs learnrust"
-abbr --add rytest "rye run pytest"
